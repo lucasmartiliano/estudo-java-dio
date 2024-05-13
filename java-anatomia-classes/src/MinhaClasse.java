7bf14600abb854460dbe19b7c71b0eb59c75a389
João Lucas Martiliano da Silva <lucasmartiliano16@gmail.com>
2024-05-13 15:32:03 -0300

    Estudo de classe

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,13 @@
+public class MinhaClasse {
+    
+    public static void main (String [] args) {
+        String primeiroNome = "João";
+        String segundoNome = "Lucas";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
